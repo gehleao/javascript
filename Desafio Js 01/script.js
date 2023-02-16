57449f875e1318c4d00c1b4ad01282a90914d182
Geysa Leão <102380954+gehleao@users.noreply.github.com>
2023-02-16 17:18:38 -0300

    Exercício 02 JS

--- Desafio Js 01/script.js
+++ Desafio Js 01/script.js
@@ -6,15 +6,15 @@ var hora = data.getHours()
 //var hora = 19
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12) {
-    //bom dia!!!
+    //Bom dia!!!
     img.src = './imagens/manha.png'
     document.body.style.background = '#e0bb89'
 } else if (hora >= 12 && hora < 18) {
-    //boa tarde!!!
+    //Boa tarde!!!
     img.src = './imagens/tarde.png'
     document.body.style.background = '#8a573a'
 } else {
-    //boa noite!!!
+    //Boa noite!!!
     img.src = './imagens/noite.png'
     document.body.style.background = '#263758'
 }
