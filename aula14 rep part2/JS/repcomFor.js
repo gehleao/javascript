1c3f08927e99529982d48769b575254458faf713
Geysa Leão <102380954+gehleao@users.noreply.github.com>
2023-03-03 08:11:28 -0300

    Aula 14
    Repetições

--- aula14 rep part2/JS/repcomFor.js
+++ aula14 rep part2/JS/repcomFor.js
@@ -1,5 +1,14 @@
 console.log('Vai começar...')
-for(var c = 1; c <= 5; c++) {
-    console.log(c)
-}
+for(var c = 1; c <= 5; c++) { 
+}   console.log(c)
 console.log('Fim!')
+//contagem crescente
+
+
+
+// console.log('Vai começar...')
+// for(var c = 1; c <= 5; c--) { 
+//     console.log(c)
+// }
+// console.log('Fim!')
+//contagem decrescente
